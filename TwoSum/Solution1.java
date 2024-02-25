6758fb20e423044584d95911517946413ea97be2
RALA PREMANSHU SINGH <98060835+ralapremanshusingh@users.noreply.github.com>
2024-02-25 15:52:29 +0530

    Two Sum

---
+++ TwoSum/Solution1.java
@@ -0,0 +1,28 @@
+package TwoSum;
+class Solution {
+    static int arr[] = {1,4,5,6};
+    static int target =7;
+
+    static int[] twoSum(int[] nums,int target) {
+
+        int n = nums.length;
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                if(nums[i]+nums[j]==target) {
+                    return new int[] {i,j};
+                }
+            }
+        }
+
+        return new int[] {};
+    } 
+
+    public static void main (String args[]) {
+        int arrs[] = {};
+        arrs = twoSum(arr, target);
+        for (int i : arrs) {
+            System.out.print(i+" ");
+        }
+    }
+}
---
+++ TwoSum/Solution2.js
@@ -0,0 +1,16 @@
+let arr =[3,2,4,5,7];
+let targ= 9;
+
+function twoSum(nums,target) {
+    let n = arr.length;
+    for(let i=0; i<n-1; i++) {
+        for(let j=i+1; j<n; j++){
+            if(arr[i]+arr[j]==target) {
+                return [i,j]
+            }
+        }
+    }
+    return null;
+}
+
+console.log(twoSum(arr,targ));
