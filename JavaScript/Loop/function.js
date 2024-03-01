0b04bbd590b34d4ec57c6ed4b2d5e38870b31a2f
RALA PREMANSHU SINGH <98060835+ralapremanshusingh@users.noreply.github.com>
2024-03-01 14:31:48 +0530

    Path Change

---
+++ JavaScript/Loop/function.js
@@ -0,0 +1,20 @@
+// var name2 =
+// var name3
+// let name4 wrong
+
+
+function human() {
+    let name1 = 'sonu'
+    var name2 = 'rohit'
+
+    console.log(name1 + name2);
+}
+
+human()
+
+console.log(name1);
+console.log(name2);
+
+var name2 = "mohan"
+
+let name4
