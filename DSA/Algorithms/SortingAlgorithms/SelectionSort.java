24f92b878d919366291bfdd08605423775546a43
RALA PREMANSHU SINGH <98060835+ralapremanshusingh@users.noreply.github.com>
2024-03-08 00:20:26 +0530

    Selection Sort

--- DSA/Algorithms/SortingAlgorithms/SelectionSort.java
+++ DSA/Algorithms/SortingAlgorithms/SelectionSort.java
@@ -1,48 +1,50 @@
 package Algorithms.SortingAlgorithms;
 
-// Java program for implementation of Selection Sort 
+import Array.ModifyArray.ModifyArray;
 
-import java.io.*; 
-public class SelectionSort 
-{ 
-	void sort(int arr[]) 
-	{ 
-		int n = arr.length; 
+public class SelectionSort { 
 
-		// One by one move boundary of unsorted subarray 
-		for (int i = 0; i < n-1; i++) 
-		{ 
-			// Find the minimum element in unsorted array 
-			int min_idx = i; 
-			for (int j = i+1; j < n; j++) 
-				if (arr[j] < arr[min_idx]) 
-					min_idx = j; 
+	public static void main(String[] args) {
 
-			// Swap the found minimum element with the first 
-			// element 
-			int temp = arr[min_idx]; 
-			arr[min_idx] = arr[i]; 
-			arr[i] = temp; 
-		} 
-	} 
+		System.out.println();
+		int[] element = ModifyArray.inputArray();
+		
+		System.out.println();
+		System.out.println("Before Sorted");
+		ModifyArray.printArray(element);
 
-	// Prints the array 
-	void printArray(int arr[]) 
-	{ 
-		int n = arr.length; 
-		for (int i=0; i<n; ++i) 
-			System.out.print(arr[i]+" "); 
-		System.out.println(); 
-	} 
+		element = selectionSort(element);
 
-	// Driver code to test above 
-	public static void main(String args[]) 
-	{ 
-		SelectionSort ob = new SelectionSort(); 
-		int arr[] = {64,25,12,22,11}; 
-		ob.sort(arr); 
-		System.out.println("Sorted array"); 
-		ob.printArray(arr); 
-	} 
+		System.out.println("\n");
+		System.out.println("After Sorted : ");
+		ModifyArray.printArray(element);
+		System.out.println("\n");
+	}
+
+	private static int[] selectionSort(int[] element) {
+
+		int n = element.length;
+
+		for(int i = 0; i<n; i++) {
+
+			int minIndex = i;
+
+			for(int j= i+1; j<n; j++) {
+				
+				if(element[minIndex] > element[j]) {
+					minIndex=j;
+				}
+			}
+
+			if(minIndex!=i) {
+				element[minIndex] = element[minIndex] + element[i];
+				element[i] = element[minIndex] - element[i];
+				element[minIndex] = element[minIndex] - element[i];
+			}
+		}
+		
+		return element;
+	}
+	
 }
 
