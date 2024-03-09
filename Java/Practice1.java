0cadd0b0021dbea33e05ecd6a95ad67cbad6f249
RALA PREMANSHU SINGH <98060835+ralapremanshusingh@users.noreply.github.com>
2024-03-09 18:01:31 +0530

    dsa

--- Java/Practice1.java
+++ Java/Practice1.java
@@ -1,24 +1,13 @@
 package Java;
 
-public class Practice1 extends Demo1{
+public class Practice1 {
 
-    Practice1() {
-        super(44);
-    }
-
-    
-        
     public static void main(String[] args) {
-       Practice1 p1 = new Practice1();
-    }
-}
+        
 
-class Demo1 {
-    // Demo1() {
-    //     System.out.println("One");
-    // }
-
-    Demo1(int a) {
-        System.out.println(a);
+        int[] arr = [1,2,3,4,5];
+        
     }
+
+   
 }
